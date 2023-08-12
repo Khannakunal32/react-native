d996e8bb3317c915c693f9cdd9f869271350cee8
Khannakunal32 <khannakunal32@gmail.com>
2023-08-12 10:32:39 +0530

    added ways to create function

--- AwesomeProject3/App.tsx
+++ AwesomeProject3/App.tsx
@@ -20,6 +20,27 @@ const style = StyleSheet.create({
   },
 });
 
+// 1st way
+function Ex(): JSX.Element {
+  return <Text>Show This</Text>;
+}
+
+// 2nd way
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function EX2(): void {
+  console.log('xyz2');
+}
+
+// 3rd way
+const Ex3 = () => {
+  console.log('xyz3');
+};
+
+// 4th way
+function fruit() {
+  return 'apple';
+}
+
 function App(): JSX.Element {
   return (
     <SafeAreaView>
@@ -27,6 +48,8 @@ function App(): JSX.Element {
       <ScrollView>
         {/* eslint-disable-next-line react-native/no-inline-styles */}
         <Text style={{fontSize: 30}}>Test is in control</Text>
+        <Ex />
+        <Text>{fruit()}</Text>
         <Text style={style.text}>Test is in control</Text>
       </ScrollView>
     </SafeAreaView>
