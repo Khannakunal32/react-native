cdb7f7d04f1e366d31c7327aaf6f52de94f67453
Khannakunal32 <khannakunal32@gmail.com>
2023-08-12 15:18:14 +0530

    added pass hide unhide

--- AwesomeProject3/components/InputBox.tsx
+++ AwesomeProject3/components/InputBox.tsx
@@ -1,20 +1,71 @@
-import {View, Text, TextInput, StyleSheet, Button} from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
 import React, {useState} from 'react';
 
 const InputBox = () => {
-  const [name, setName] = useState('kunal');
-  function changeHandler(val: any): void {
-    setName(val);
-  }
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [display, setDisplay] = useState(false);
+  const [veiw, setView] = useState(false);
   return (
     <View>
       <Text style={{fontSize: 35}}>{name}</Text>
       <TextInput
         style={style.input}
         value={name}
-        onChangeText={changeHandler}
+        placeholder="username"
+        onChangeText={text => {
+          setName(text);
+        }}
       />
-      <Button title="clear" onPress={() => setName('')} />
+      <View style={{display: 'flex', flexDirection: 'row'}}>
+        <TextInput
+          style={[style.input, {flex: 2}]}
+          value={password}
+          secureTextEntry={!veiw}
+          placeholder="password"
+          onChangeText={text => {
+            setPassword(text);
+          }}
+        />
+        <TouchableOpacity style={{flex: 1}}>
+          <Text
+            style={{
+              flex: 1,
+              textAlign: 'center',
+              textAlignVertical: 'center',
+              backgroundColor: 'blue',
+              color: 'white',
+            }}
+            onPress={() => {
+              veiw ? setView(false) : setView(true);
+            }}>
+            View
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Button
+        title="show"
+        onPress={() => {
+          display ? setDisplay(false) : setDisplay(true);
+        }}
+      />
+
+      <Button title="clear" onPress={() => [setName(''), setPassword('')]} />
+      {display ? (
+        <View>
+          <Text style={{fontSize: 35}}>{name}</Text>
+          <Text style={{fontSize: 35}}>{password}</Text>
+        </View>
+      ) : (
+        <Text />
+      )}
     </View>
   );
 };
